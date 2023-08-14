cb3e7c69960c929a8fd7f522f42687a9402b6890
Mykyta Machekhin <machekhinn@gmail.com>
2023-08-14 00:55:19 -0400

    Add DB connection

---
+++ prisma/migrations/20230814045347_/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "accountId" TEXT NOT NULL,
+    "externalId" TEXT NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("accountId")
+);
--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,7 +1,4 @@
-import {Controller} from "@nestjs/common";
+import { Controller } from '@nestjs/common';
 
 @Controller('account')
-export class AccountController {
-
-}
-
+export class AccountController {}
